b95cec9fa7eb361795406186389f3811967a3a99
Tais8675 <116527867+Tais8675@users.noreply.github.com>
2023-01-31 11:07:54 -0300

    Add files via upload

---
+++ web2 _27_01/teste.js
@@ -0,0 +1,15 @@
+var n1_1 = document.querySelector('#n1_1')
+var n1_2 = document.querySelector('#n1_2')
+var n2_1 = document.querySelector('#n2_1')
+var n2_2 = document.querySelector('#n2_2')
+var N1 = (n1_1 + n1_2)*2
+var n1 = (N1/2)-30
+var N2 = (n2_1+ n2_2)
+var n2 = (N2*3)
+var n = (n1+n2)
+var resultado = document.querySelector('span')
+
+
+
+function  calcular () {
+    resultado.innerHTML = (n/5 )}
